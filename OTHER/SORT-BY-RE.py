feaffadf0095df24ca45b1de4ec5a4ac806ed39c
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-16 02:44:51 -0500

    Add files via upload

---
+++ OTHER/SORT-BY-RE.py
@@ -0,0 +1,14 @@
+import csv
+import re
+
+media = csv.reader(open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv"))
+
+
+def get_media_years():
+    for info in media:
+        media_year = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(info))
+        print(media_year[0])
+
+
+get_media_years()
+
