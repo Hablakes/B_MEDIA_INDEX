4a8f3bd7b968d62c92af4cb2e92f65166855fa55
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-16 13:34:28 -0500

    Add files via upload

--- CODE/LAUNCH-MEDIA-INDEX.py
+++ CODE/LAUNCH-MEDIA-INDEX.py
@@ -3,10 +3,9 @@ import os
 
 import matplotlib.pylab as plt
 
-
 movies_dir = os.listdir(r"/home/bx/Videos/CHASE/MOVIES/")
 tv_dir = os.listdir(r"/home/bx/Videos/CHASE/TV/")
-#tv2_dir = os.listdir(r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-tv-2/TV-2/")
+# tv2_dir = os.listdir(r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-tv-2/TV-2/")
 
 movie_years_range = range(1900, 2100, 1)
 tv_show_years_range = range(1900, 2100, 1)
@@ -33,7 +32,7 @@ def tv_search(lower_title_search, tv_dir):
             continue
 
 
-#def tv2_search(lower_title_search, tv2_dir):
+# def tv2_search(lower_title_search, tv2_dir):
 #    for tv_result in tv2_dir:
 #        if lower_title_search in tv_result.lower():
 #            print(tv_result)
@@ -86,6 +85,8 @@ def run_base():
     print("TV")
     print()
     tv_search(lower_title_search, sorted(tv_dir))
+
+
 #    tv2_search(lower_title_search, sorted(tv2_dir))
 
 
@@ -94,7 +95,7 @@ def run_sort():
     print("|__] __ |\/| |___ |  \ | |__| __ [__  |  | |__/  |")
     print("|__]    |  | |___ |__/ | |  |    ___] |__| |  \  | ")
     print()
-    print("METHOD? - TITLE - 1) ASCENDING 2) DESCENDING - YEAR - 3)ASCENDING 4) DESCENDING - 5) EXIT")
+    print("TITLE - 1) ASCENDING 2) DESCENDING - YEAR - 3)ASCENDING 4) DESCENDING - 5) EXIT")
     print()
     sort_options = input("ENTER #")
     print()
@@ -119,7 +120,7 @@ def tv_listdir():
     return os.listdir(r"/home/bx/Videos/CHASE/TV/")
 
 
-#def tv2_listdir():
+# def tv2_listdir():
 #    return os.listdir(r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-tv-2/TV-2/")
 
 
@@ -135,10 +136,10 @@ def create_media_index_csv():
         tv_year = tv.strip()[-5:-1]
         found.append(["TV", tv_title, tv_year])
 
-#    for tv2 in tv2_listdir():
-#        tv2_title = tv2.strip()[0:-7]
-#        tv2_year = tv2.strip()[-5:-1]
-#        found.append(["TV", tv2_title, tv2_year])
+    #    for tv2 in tv2_listdir():
+    #        tv2_title = tv2.strip()[0:-7]
+    #        tv2_year = tv2.strip()[-5:-1]
+    #        found.append(["TV", tv2_title, tv2_year])
 
     found = sorted(found)
     with open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv", "w", newline="") as f:
@@ -157,7 +158,7 @@ def get_movie_years_for_dict():
     media_movie_year_totals = {}
     for movie_year_values, value in sorted(movie_years_dict.items()):
         media_movie_year_totals[movie_year_values] = len(value)
-#   print(media_movie_year_totals)
+    #   print(media_movie_year_totals)
     movie_data = sorted(media_movie_year_totals.items())
 
     x, y = zip(*movie_data)
@@ -177,7 +178,7 @@ def get_tv_years_for_dict():
     media_tv_year_totals = {}
     for tv_year_values, value in sorted(tv_show_years_dict.items()):
         media_tv_year_totals[tv_year_values] = len(value)
-#   print(media_tv_year_totals)
+    #   print(media_tv_year_totals)
     tv_data = sorted(media_tv_year_totals.items())
 
     x, y = zip(*tv_data)
@@ -192,7 +193,7 @@ def run_graphs():
     print("|__] __ | __ |__/ |__| |__] |__| __ |  | |__]  |  | |  | |\ | [__")
     print("|__]    |__] |  \ |  | |    |  |    |__| |     |  | |__| | \| ___]")
     print()
-    print("ACTION? - 1) MOVIES (TITLES PER YEAR) - 2) TV SHOWS (TITLES PER YEAR) - 3) EXIT")
+    print("1) MOVIES (TITLES PER YEAR) - 2) TV SHOWS (TITLES PER YEAR) - 3) EXIT")
     print()
     graph_options = input("ENTER #")
     print()
@@ -210,20 +211,20 @@ def launch_media_index():
     print(" |__] __ |\/| |___ |  \ | |__| __ | |\ | |  \ |___  \/")
     print(" |__]    |  | |___ |__/ | |  |    | | \| |__/ |___ _/\_")
     print()
-    print("ACTION? - 1) QUERY INDEX - 2) SORT OPTIONS - 3) GRAPHS - 4) RE-SCAN INDEX - 5) EXIT")
+    print("1) QUERY INDEX - 2) SORT OPTIONS - 3) GRAPHS - 4) RE-SCAN INDEX - 5) EXIT")
     print()
-    action = input("ENTER #")
+    lmi_action = input("ENTER #")
     print()
-    action = int(action)
-    if action == 1:
+    lmi_action = int(lmi_action)
+    if lmi_action == 1:
         run_base()
-    elif action == 2:
+    elif lmi_action == 2:
         run_sort()
-    elif action == 3:
+    elif lmi_action == 3:
         run_graphs()
-    elif action == 4:
+    elif lmi_action == 4:
         create_media_index_csv()
-    elif action == 5:
+    elif lmi_action == 5:
         exit()
 
 
