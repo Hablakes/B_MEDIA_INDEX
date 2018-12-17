6f992de33891b290edca90588b116b1290dc7ed0
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-16 19:33:09 -0500

    Add files via upload

---
+++ OTHER/TOTALS-QUERY.py
@@ -0,0 +1,25 @@
+
+def totals_query():
+    print("___     ___ ____ ___ ____ _    ____    ____ _  _ ____ ____ _   _")
+    print("|__] __  |  |  |  |  |__| |    [__  __ |  | |  | |___ |__/  \_/ ")
+    print("|__]     |  |__|  |  |  | |___ ___]    |_\| |__| |___ |  \   |   ")
+    print()
+    print("1) MOVIE BY YEAR - 2) MOVIES TOTAL - 3) TV SHOWS BY YEAR 4) TV SHOWS TOTAL - 5) EXIT")
+    print()
+    totals_query_action = input("ENTER #")
+    print()
+    totals_query_action = int(totals_query_action)
+
+
+"""    
+    if totals_query_action == 1:
+
+    elif totals_query_action == 2:
+
+    elif totals_query_action == 3:
+
+    elif totals_query_action == 4:
+
+    elif totals_query_action == 5:
+        exit()
+"""
