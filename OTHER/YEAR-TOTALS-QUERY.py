d55627ff6bc0398275c005bafb3199889fabca42
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-16 21:29:19 -0500

    Add files via upload

--- OTHER/YEAR-TOTALS-QUERY.py
+++ OTHER/YEAR-TOTALS-QUERY.py
@@ -16,11 +16,28 @@ tv_years_dict = {}
 movie_string = str("MOVIE")
 tv_string = str("TV")
 
-totals_query_action = input("ENTER #")
-totals_query_action = int(totals_query_action)
+
+def totals_query():
+    print("___     ___ ____ ___ ____ _    ____    ____ _  _ ____ ____ _   _")
+    print("|__] __  |  |  |  |  |__| |    [__  __ |  | |  | |___ |__/  \_/ ")
+    print("|__]     |  |__|  |  |  | |___ ___]    |_\| |__| |___ |  \   |   ")
+    print()
+    print("1) MOVIES BY YEAR - 2) MOVIES TOTAL - 3) TV SHOWS BY YEAR 4) TV SHOWS TOTAL - 5) EXIT")
+    print()
+    b_totals_query_action = input("ENTER #")
+    print()
+    b_totals_query_action = int(b_totals_query_action)
+    if b_totals_query_action == 1:
+        get_movie_years_for_dict()
+    elif b_totals_query_action == 3:
+        get_tv_years_for_dict()
+    elif b_totals_query_action == 5:
+        exit()
 
 
 def get_movie_years_for_dict():
+    movie_totals_query_action = input("ENTER #")
+    movie_totals_query_action = int(movie_totals_query_action)
     for media_movie in media_index:
         media_movie_year = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(media_movie), flags=0)
         media_movie_year = int(media_movie[2])
@@ -34,9 +51,30 @@ def get_movie_years_for_dict():
         media_movie_year_totals[movie_year_values] = len(value)
     #   print(media_movie_year_totals)
     movie_data = sorted(media_movie_year_totals.items())
-    for year_query in movie_data:
-        if totals_query_action in year_query:
-            print(year_query[1])
+    for movie_year_query in movie_data:
+        if movie_totals_query_action in movie_year_query:
+            print(movie_year_query[1])
 
 
-get_movie_years_for_dict()
+def get_tv_years_for_dict():
+    tv_totals_query_action = input("ENTER #")
+    tv_totals_query_action = int(tv_totals_query_action)
+    for media_tv in media_index:
+        media_tv_year = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(media_tv), flags=0)
+        media_tv_year = int(media_tv[2])
+        if tv_string in media_tv:
+            if media_tv_year in tv_years_range:
+                if media_tv_year not in tv_years_dict:
+                    tv_years_dict[media_tv_year] = []
+                tv_years_dict[media_tv_year].append(media_tv)
+    media_tv_year_totals = {}
+    for tv_year_values, value in sorted(tv_years_dict.items()):
+        media_tv_year_totals[tv_year_values] = len(value)
+    #   print(media_tv_year_totals)
+    tv_data = sorted(media_tv_year_totals.items())
+    for tv_year_query in tv_data:
+        if tv_totals_query_action in tv_year_query:
+            print(tv_year_query[1])
+
+
+totals_query()
