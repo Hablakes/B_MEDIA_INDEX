d028be7ca1dfb77783fd57c8d62c5f3851106669
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-18 01:10:55 -0500

    Add files via upload

--- CODE/CURRENT-TEST.py
+++ CODE/CURRENT-TEST.py
@@ -15,15 +15,40 @@ tv_title_results = []
 tv_year_results = []
 
 
-def title_search():
-    for search_result in media_index_list:
-        search_section_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(search_result[0]), flags=0)
-        search_title_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(search_result[1]), flags=0)
-        search_year_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(search_result[2]), flags=0)
-        print()
+def run_title_search():
+    if title_search_type_lower == 1:
+        movie_title_search()
+    elif title_search_type_lower == 2:
+        tv_title_search()
+    elif title_search_type_lower == 3:
+        exit()
+
+
+def movie_title_search():
+    for movie_search_result in media_index_list:
+        if movie_string in movie_search_result[0]:
+            movie_search_section_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(movie_search_result[0]), flags=0)
+            movie_search_title_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(movie_search_result[1]), flags=0)
+            movie_search_year_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(movie_search_result[2]), flags=0)
+            print(movie_search_title_info[0])
 
 
 
+def tv_title_search():
+    for tv_search_result in media_index_list:
+        if tv_string in tv_search_result:
+            tv_search_section_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(tv_search_result[0]), flags=0)
+            tv_search_title_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(tv_search_result[1]), flags=0)
+            tv_search_year_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(tv_search_result[2]), flags=0)
+            print(tv_search_title_info[0])
 
 
-
+print()
+print("SEARCH TITLES - 1) MOVIES 2) TV SHOWS - 3) EXIT")
+title_search_type = input("ENTER #")
+title_search_type_lower = title_search_type.lower()
+print()
+title_search_action = input("QUERY?")
+title_search_action_lower = title_search_action.lower()
+print()
+movie_title_search()
