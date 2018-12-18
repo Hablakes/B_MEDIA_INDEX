37bfdd06b1cda40aee55dbf25cd2e3a21b85559a
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-18 00:24:22 -0500

    Add files via upload

--- CODE/CURRENT-TEST.py
+++ CODE/CURRENT-TEST.py
@@ -1,1 +1,29 @@
+import csv
+import re
+
+media_index = csv.reader(open(r'C:/Users/botoole/Downloads/B/BPT/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv'))
+media_index_list = list(csv.reader(open(r'C:/Users/botoole/Downloads/B/BPT/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv')))
+
+movie_string = str("MOVIE")
+tv_string = str("TV")
+
+movie_section_results = []
+movie_title_results = []
+movie_year_results = []
+tv_section_results = []
+tv_title_results = []
+tv_year_results = []
+
+
+def title_search():
+    for search_result in media_index_list:
+        search_section_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(search_result[0]), flags=0)
+        search_title_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(search_result[1]), flags=0)
+        search_year_info = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(search_result[2]), flags=0)
+        print()
+
+
+
+
+
 
