2bbbbb0a1ab9778fd4869a610efc6935d9d39da5
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-19 12:35:29 -0500

    Add files via upload

--- CODE/LIST-MEDIA-FILES-IN-DIRECTORY.py
+++ CODE/LIST-MEDIA-FILES-IN-DIRECTORY.py
@@ -1,23 +1,19 @@
 import os
+import re
 
 movie_dir = r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-movies/MOVIES/"
 
-movie_file_results = []
+movie_file_results_dict = {}
 
 
 def search_movie_folders_items():
     for root, dirs, files in os.walk(movie_dir):
         for movie in files:
-            for extension in [".avi", ".divx", ".img", ".iso," ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".qt", ".webm",
-                              ".wmv", ".xvid"]:
-
+            for extension in [".3gp", ".avi", ".divx", ".img", ".iso," ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".qt",
+                              ".webm", ".wmv", ".xvid", ".srt"]:
                 if movie.endswith(extension):
-                    movie_file_results.append(os.path.join(movie))
-"""
-                    print(os.path.join(movie_dir, movie))
-"""
+                    movie_file_info = re.search("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(movie), flags=0)
+                    print(movie_file_info)
 
 
 search_movie_folders_items()
-
-print(sorted(movie_file_results))
