3955b8b0019f7f6c850665365e513b01253bec10
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-19 08:03:36 -0500

    Add files via upload

---
+++ OTHER/MEDIA-INDEX-BASE.py
@@ -0,0 +1,58 @@
+import os
+
+movies_dir = os.listdir(r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-movies/MOVIES/")
+tv_dir = os.listdir(r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-tv/TV/")
+tv2_dir = os.listdir(r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-tv-2/TV-2/")
+
+
+def movie_search(lower_title_search, movies_dir):
+    for movie_result in movies_dir:
+        if lower_title_search in movie_result.lower():
+            print(movie_result)
+        else:
+            continue
+
+
+def tv_search(lower_title_search, tv_dir):
+    for tv_result in tv_dir:
+        if lower_title_search in tv_result.lower():
+            print(tv_result)
+        else:
+            continue
+
+
+def tv2_search(lower_title_search, tv2_dir):
+    for tv_result in tv2_dir:
+        if lower_title_search in tv_result.lower():
+            print(tv_result)
+        else:
+            continue
+
+
+def run_base():
+    print()
+    print(" ___     _  _ ____ ___  _ ____    _ _  _ ___  ____ _  _")
+    print(" |__] __ |\/| |___ |  \ | |__| __ | |\ | |  \ |___  \/")
+    print(" |__]    |  | |___ |__/ | |  |    | | \| |__/ |___ _/\_")
+    print()
+    print()
+    print("MEDIA SEARCH:")
+    print()
+    title_search = input("QUERY?")
+    print()
+    lower_title_search = title_search.lower()
+    print()
+    print("MOVIES")
+    print()
+    movie_search(lower_title_search, sorted(movies_dir))
+    print()
+    print("TV")
+    print()
+    tv_search(lower_title_search, sorted(tv_dir))
+    tv2_search(lower_title_search, sorted(tv2_dir))
+
+
+while True:
+    run_base()
+    print()
+    print()
