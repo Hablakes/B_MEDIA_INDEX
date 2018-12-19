399650086214a9430f1c07c90bc82ebac603d8e3
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-19 12:01:12 -0500

    Add files via upload

--- CODE/LIST-MEDIA-FILES-IN-DIRECTORY.py
+++ CODE/LIST-MEDIA-FILES-IN-DIRECTORY.py
@@ -1,16 +1,23 @@
 import os
 
-movie_dir = r"C:\Users\botoole\Downloads\B\BTMP\TEST\MOVIES"
+movie_dir = r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-movies/MOVIES/"
+
+movie_file_results = []
 
 
 def search_movie_folders_items():
     for root, dirs, files in os.walk(movie_dir):
         for movie in files:
-            for extension in [".avi", ".divx", ".img", ".iso," ".m4v", ".mov", ".mp4", ".mpeg", ".qt", ".webm", ".wmv",
-                              ".xvid"]:
+            for extension in [".avi", ".divx", ".img", ".iso," ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".qt", ".webm",
+                              ".wmv", ".xvid"]:
 
                 if movie.endswith(extension):
+                    movie_file_results.append(os.path.join(movie))
+"""
                     print(os.path.join(movie_dir, movie))
+"""
 
 
 search_movie_folders_items()
+
+print(sorted(movie_file_results))
