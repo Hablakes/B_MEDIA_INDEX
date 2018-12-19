cb8069bbe0237a1c916b4a4f15393bba41c84560
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-19 01:18:34 -0500

    Add files via upload

--- CODE/YEAR-TOTALS-GRAPHS.py
+++ CODE/YEAR-TOTALS-GRAPHS.py
@@ -1,53 +1,60 @@
-import os
+import csv
+import re
 
 import matplotlib.pylab as plt
 
-movie_p_dir = os.listdir(r"/home/bx/Videos/CHASE/MOVIES/")
-tv_p_dir = os.listdir(r"/home/bx/Videos/CHASE/TV/")
+media_index = list(csv.reader(open(r"C:/Users/botoole/Downloads/B/BPT/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv")))
+media_index_list = list(csv.reader(open(r"C:/Users/botoole/Downloads/B/BPT/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv")))
 
 movie_years_range = range(1900, 2100, 1)
 tv_show_years_range = range(1900, 2100, 1)
 
 movie_years_dict = {}
-tv_show_years_dict = {}
+tv_years_dict = {}
+
+movie_string = str("MOVIE")
+tv_string = str("TV")
 
 
 def get_movie_years_for_dict():
-    for media_movie in movie_p_dir:
-        media_movie_year = int(media_movie.strip()[-5:-1])
-        if media_movie_year in movie_years_range:
-            if media_movie_year not in movie_years_dict:
-                movie_years_dict[media_movie_year] = []
-            movie_years_dict[media_movie_year].append(media_movie)
+    for media_movie in media_index:
+        media_movie_year = re.split("(.+) \((\d{4})\)", media_movie[2], flags=0)
+        media_movie_year_int = int(media_movie_year[0])
+        if movie_string in media_movie:
+            if media_movie_year_int in movie_years_range:
+                if media_movie_year_int not in movie_years_dict:
+                    movie_years_dict[media_movie_year_int] = []
+                movie_years_dict[media_movie_year_int].append(media_movie)
     media_movie_year_totals = {}
     for movie_year_values, value in sorted(movie_years_dict.items()):
         media_movie_year_totals[movie_year_values] = len(value)
-    # print(media_movie_year_totals)
     movie_data = sorted(media_movie_year_totals.items())
 
     x, y = zip(*movie_data)
 
     plt.bar(x, y)
-    plt.savefig(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MOVIE-YEAR-RESULTS.png')
+    plt.savefig(r'C:\Users\botoole\Downloads\B\BPT\B-MEDIA-INDEX\FILES\MOVIE-YEAR-RESULTS.png')
     plt.show()
 
 
 def get_tv_years_for_dict():
-    for media_tv in tv_p_dir:
-        media_tv_year = int(media_tv.strip()[-5:-1])
-        if media_tv_year in tv_show_years_range:
-            if media_tv_year not in tv_show_years_dict:
-                tv_show_years_dict[media_tv_year] = []
-            tv_show_years_dict[media_tv_year].append(media_tv)
+    for media_tv in media_index:
+        media_tv_year = re.split("(.+) \((\d{4})\)", media_tv[2], flags=0)
+        media_tv_year_int = int(media_tv_year[0])
+        if movie_string in media_tv:
+            if media_tv_year_int in movie_years_range:
+                if media_tv_year_int not in tv_years_dict:
+                    tv_years_dict[media_tv_year_int] = []
+                tv_years_dict[media_tv_year_int].append(media_tv)
     media_tv_year_totals = {}
-    for tv_year_values, value in sorted(tv_show_years_dict.items()):
+    for tv_year_values, value in sorted(tv_years_dict.items()):
         media_tv_year_totals[tv_year_values] = len(value)
     tv_data = sorted(media_tv_year_totals.items())
 
     x, y = zip(*tv_data)
 
     plt.bar(x, y)
-    plt.savefig(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/TV-YEAR-RESULTS.png')
+    plt.savefig(r'C:\Users\botoole\Downloads\B\BPT\B-MEDIA-INDEX\FILES\TV-YEAR-RESULTS.png')
     plt.show()
 
 
