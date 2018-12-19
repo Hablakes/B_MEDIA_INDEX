a6e443330332ccb38c9d04cb643ee8a1c15cb692
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-19 07:48:48 -0500

    Add files via upload

--- NOTES/B-NOTES.py
+++ NOTES/B-NOTES.py
@@ -4,10 +4,38 @@ PIP INSTALLS -
 UPGRADE **
 
 click
+get-media-files (AddOn for pymediainfo)
 matplotlib
 pyfiglet
+pymediainfo (IF STUCK, making our own version of this is the point)
 
-FOR *-YEARS-GRAPH //  Use Terminal: sudo pacman -S tk (on Arch based systems)
 
-MISC - Install // lolcat, cool-retro-term
+
+OS INSTALLS -
+
+Use Terminal: sudo pacman -S tk (on Arch based systems)
+
+
+
+MISC -
+
+Install // lolcat, cool-retro-term
+
+
+
+DIRS -
+
+NIX
+media_index = list(csv.reader(open(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv')))
+media_index_list = list(csv.reader(open(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv')))
+
+movies_dir = os.listdir(r"/home/bx/Videos/CHASE/MOVIES/")
+tv_dir = os.listdir(r"/home/bx/Videos/CHASE/TV/")
+
+WIN
+media_index = list(csv.reader(open(r"C:/Users/botoole/Downloads/B/BPT/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv")))
+media_index_list = list(csv.reader(open(r"C:/Users/botoole/Downloads/B/BPT/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv")))
+
+movies_dir = os.listdir(r"C:/Users/botoole/Downloads/B/BTMP/CHASE/MOVIES/")
+tv_dir = os.listdir(r"C:/Users/botoole/Downloads/B/BTMP/CHASE/TV/")
 """
