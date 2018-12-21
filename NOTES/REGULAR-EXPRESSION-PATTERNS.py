447bb681f8e2d20c868297b48500bd61f82c25f3
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-21 02:02:00 -0500

    Add files via upload

--- NOTES/REGULAR-EXPRESSION-PATTERNS.py
+++ NOTES/REGULAR-EXPRESSION-PATTERNS.py
@@ -1,5 +1,5 @@
 """
-_________________________________________________________________________
+______________________________________________________________________________________________________________________
 
 patterns = [
     ('season', '(s?([0-9]{1,2}))[ex]'),
@@ -12,7 +12,7 @@ types = {
     'episode': 'integer',
     'year': 'integer',
 }
-_________________________________________________________________________
+______________________________________________________________________________________________________________________
 
 MY RE -
 
@@ -20,47 +20,50 @@ MY RE -
       (?P<Titles>[^\(]+)[\s.]
 
     | # Year
-      (\((?P<Year>\d{4})\)
-    )
+      (\((?P<Year>\d{4})\))
     | # Old_Resolution_Standard
-      (\((?P<Old_Resolution_Standard>\d{3,}p)\)
-    )
+      (\((?P<Old_Resolution_Standard>\d{3,}p)\))
     | # Resolution_Standard
-      (?P<Resolution_Standard>\d+x\d+
-    )
-    | # HD_Resolution Style Resolution (Oldest)
-      (\((?P<HD_Resolution>\wD)\)
-    )
+      (\((?P<Resolution_Standard>\d+x\d+)\))
+    | # HD_SD_Resolution Style Resolution
+      (\((?P<HD_SD_Resolution>\wD)\))
     | # Parts
-      (?P<Parts>Part\s\d{1}
-    )
+      (?P<Parts>Part\s\d{1})
     | # FileType
-      (?<=\.)(?P<FileType>[a-z0-9]{3,}
-    )
+      (?<=\.)(?P<FileType>[a-z0-9]{3,})
+______________________________________________________________________________________________________________________
 
-=========================================================================
+titles = re.split("(?P<Titles>[^\(]+)[\s.]", movie_data)
 
-^    # Titles
-      (?P<Titles>[^\(]+)[\s.]
-     # Year
-      (\((?P<Year>\d{4})\))?
-     # Resolution
-      (?P<Resolution_Standard>\(\d+x\d+\))?
-     # Old_Resolution_Standard
-      (\((?P<Old_Resolution_Standard>\d{3,}p)\))?
-     # SD_Resolution Style Resolution (Oldest)
-      (?P<SD_Resolution>\(SD\))?
+titles_wsc = re.split("(?P<Titles_W_S_C>[^\s\-]+)", movie_data)
 
-_________________________________________________________________________
+year = re.split("(\((?P<Year>\d{4})\))?", movie_data)
+
+year_ar = re.split("(\s\((?P<Year_A_R>\d{4})\))?", movie_data)
+
+resolution = re.split("(\((?P<Res_Standard>\d+x\d+)\))?", movie_data)
+
+resolution_ay = re.split("(\s\((?P<Res_A_Y>\d+x\d+)\))?", movie_data)
+
+old_res_standard = re.split("(\((?P<Old_Res_Standard>\d{3,}p)\))?", movie_data)
+
+hd_sd_res_standard = re.split("(\((?P<HD_SD_Res>[A-Z]D)\))?", movie_data)
+
+hd_sd_res_standard_as = re.split("(\s\((?P<HD_SD_Res_A_S>[A-Z]D)\))?", movie_data)
+
+parts = re.split("(\s(?P<Parts>Part\s\d{1,2}))?", movie_data)
+
+file_type = re.split("((?<=\.)(?P<FileType>[\w]{3,}))", movie_data)
+______________________________________________________________________________________________________________________
 
 YEAR - #\s[0-9][0-9][0-9][0-9]
 
 YEAR + RES - (.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)
-_________________________________________________________________________
+______________________________________________________________________________________________________________________
 
 FILE TYPES - ".3gp", ".avi", ".divx", ".img", ".iso," ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".qt", ".webm", ".wmv",
 ".xvid", ".srt"
-_________________________________________________________________________
+______________________________________________________________________________________________________________________
 
 USE CAPTURE GROUPS TO MATCH RESOLUTION IN FILENAMES
 """
