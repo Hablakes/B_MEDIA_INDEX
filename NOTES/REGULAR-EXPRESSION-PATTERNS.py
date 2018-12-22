15330688fdfc14afc08c96753030235b3d0932e9
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-21 21:35:04 -0500

    Add files via upload

--- NOTES/REGULAR-EXPRESSION-PATTERNS.py
+++ NOTES/REGULAR-EXPRESSION-PATTERNS.py
@@ -1,24 +1,29 @@
 """
+
+TITLES -
+
+(\.3gp)|(\.avi)|(\.divx)|(\.img)|(\.iso)|(\.m4a)|(\.m4v)|(\.mkv)|(\.mov)|(\.mp4)|(\.mpeg)(\.qt)(\.webm)(\.wmv)|
+(\.xvid)|(\.srt)
+
 ______________________________________________________________________________________________________________________
+
+TYPES -
 
 patterns = [
     ('season', '(s?([0-9]{1,2}))[ex]'),
     ('episode', '([ex]([0-9]{2})(?:[^0-9]|$))'),
-    ('year', '([\[\(]?((?:19[0-9]|20[01])[0-9])[\]\)]?)'),
-]
+    ('year', '([\[\(]?((?:19[0-9]|20[01])[0-9])[\]\)]?)'),]
 
 types = {
     'season': 'integer',
     'episode': 'integer',
-    'year': 'integer',
-}
+    'year': 'integer',}
 ______________________________________________________________________________________________________________________
 
 MY RE -
 
 ^     # Titles
       (?P<Titles>[^\(]+)[\s.]
-
     | # Year
       (\((?P<Year>\d{4})\))
     | # Old_Resolution_Standard
@@ -31,33 +36,21 @@ MY RE -
       (?P<Parts>Part\s\d{1})
     | # FileType
       (?<=\.)(?P<FileType>[a-z0-9]{3,})
-______________________________________________________________________________________________________________________
 
 titles = re.split("(?P<Titles>[^\(]+)[\s.]", movie_data)
-
 titles_wsc = re.split("(?P<Titles_W_S_C>[^\s\-]+)", movie_data)
-
 year = re.split("(\((?P<Year>\d{4})\))?", movie_data)
-
 year_ar = re.split("(\s\((?P<Year_A_R>\d{4})\))?", movie_data)
-
 resolution = re.split("(\((?P<Res_Standard>\d+x\d+)\))?", movie_data)
-
 resolution_ay = re.split("(\s\((?P<Res_A_Y>\d+x\d+)\))?", movie_data)
-
 old_res_standard = re.split("(\((?P<Old_Res_Standard>\d{3,}p)\))?", movie_data)
-
 hd_sd_res_standard = re.split("(\((?P<HD_SD_Res>[A-Z]D)\))?", movie_data)
-
 hd_sd_res_standard_as = re.split("(\s\((?P<HD_SD_Res_A_S>[A-Z]D)\))?", movie_data)
-
 parts = re.split("(\s(?P<Parts>Part\s\d{1,2}))?", movie_data)
-
 file_type = re.split("((?<=\.)(?P<FileType>[\w]{3,}))", movie_data)
 ______________________________________________________________________________________________________________________
 
 YEAR - #\s[0-9][0-9][0-9][0-9]
-
 YEAR + RES - (.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)
 ______________________________________________________________________________________________________________________
 
@@ -65,5 +58,6 @@ FILE TYPES - ".3gp", ".avi", ".divx", ".img", ".iso," ".m4v", ".mkv", ".mov", ".
 ".xvid", ".srt"
 ______________________________________________________________________________________________________________________
 
-USE CAPTURE GROUPS TO MATCH RESOLUTION IN FILENAMES
+*** USE CAPTURE GROUPS TO MATCH RESOLUTION IN FILENAMES
+
 """
