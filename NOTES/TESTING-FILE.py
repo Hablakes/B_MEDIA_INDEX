ecd4a352de8e53bee3d02aee2cff5b6e4dd395f3
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-24 01:37:37 -0500

    Add files via upload

--- NOTES/TESTING-FILE.py
+++ NOTES/TESTING-FILE.py
@@ -1,6 +1,5 @@
 import csv
 
-
 media_index_test = csv.reader(open(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX-TEST.csv'))
 media_index_test_list = list(csv.reader(open(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX-TEST.csv')))
 
@@ -14,10 +13,26 @@ movie_file_type = media_index_test_list[6]
 
 
 def movie_file_query_and_sort():
+    mv_query_action = input("ENTER SEARCH QUERY:")
+    print()
+    mv_query_action_lower = str(mv_query_action.lower())
     for movie_file in media_index_test_list:
-        print(movie_file)
+        if mv_query_action_lower in movie_file[0].lower():
+            print("MOVIE TITLE:")
+            print(movie_file[0])
+            print()
+            print("MOVIE YEAR:")
+            print(movie_file[1])
+            print()
+            print("MOVIE RESOLUTION:")
+            print(movie_file[2])
+            print()
+            print("MOVIE PARTS:")
+            print(movie_file[5])
+            print()
+            print("MOVIE FILE TYPE:")
+            print(movie_file[6])
+            print()
 
 
 movie_file_query_and_sort()
-
-
