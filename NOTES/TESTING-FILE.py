a438bf75516539536c40c6b08c719cd465c611b9
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-23 20:49:11 -0500

    Add files via upload

--- NOTES/TESTING-FILE.py
+++ NOTES/TESTING-FILE.py
@@ -10,31 +10,11 @@ movie_results = []
 
 def search_movie_folders_items():
     for root, dirs, movie in movie_data:
-        re_ext = re.findall(
+        re_ext = re.search(
             "(\.3gp)|(\.avi)|(\.divx)|(\.img)|(\.iso)|(\.m4a)|(\.m4v)|(\.mkv)|(\.mov)|(\.mp4)|(\.mpeg)|(\.qt)|"
             "(\.webm)|(\.wmv)|(\.xvid)", str(movie))
-        for found_movie_file in re_ext:
-            found_movie_file_format = re.search(
-                "(\.3gp)|(\.avi)|(\.divx)|(\.img)|(\.iso)|(\.m4a)|(\.m4v)|(\.mkv)|(\.mov)|(\.mp4)|(\.mpeg)|(\.qt)|"
-                "(\.webm)|(\.wmv)|(\.xvid)", str(found_movie_file))
-
-            print([movie, [found_movie_file_format[0]]])
+        print(movie, re_ext)
 
 
 search_movie_folders_items()
 
-"""
-        for movie_match in movie_walk:
-            movie_year_info = re.findall("\((\d{4})\)", str(movie_match)),
-            movie_res_info = re.findall("\((\d+x\d+)\)", str(movie_match)),
-            movie_hsd_res_standard = re.findall("\((\wD)\)", str(movie_match)),
-            movie_old_res_standard = re.findall("\((\d{3,}p)\)", str(movie_match)),
-            movie_parts = re.findall("Part\s\d{1,2}", str(movie_match)),
-            movie_file_type = re.finditer(
-                "(\.3gp)|(\.avi)|(\.divx)|(\.img)|(\.iso)|(\.m4a)|(\.m4v)|(\.mkv)|(\.mov)|(\.mp4)|(\.mpeg)|(\.qt)|"
-                "(\.webm)|(\.wmv)|(\.xvid)", str(movie_match))
-            for found_movie_match in movie_file_type:
-                movie_results.append(
-                    [movie_match, movie_year_info[0], movie_res_info[0], movie_hsd_res_standard[0],
-                     movie_old_res_standard[0], movie_parts[0], [found_movie_match[0]]])
-"""
