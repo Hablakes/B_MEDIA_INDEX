ffd265ca0f97e3112813dcfbb0d7811f5cb7c340
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-24 01:19:18 -0500

    Add files via upload

--- NOTES/TESTING-FILE.py
+++ NOTES/TESTING-FILE.py
@@ -1,20 +1,23 @@
 import csv
-import os
-import re
-
-movie_data = os.walk(r"/home/bx/Videos/CHASE/TEST/")
-
-movie_walk = []
-movie_results = []
 
 
-def search_movie_folders_items():
-    for root, dirs, movie in movie_data:
-        re_ext = re.search(
-            "(\.3gp)|(\.avi)|(\.divx)|(\.img)|(\.iso)|(\.m4a)|(\.m4v)|(\.mkv)|(\.mov)|(\.mp4)|(\.mpeg)|(\.qt)|"
-            "(\.webm)|(\.wmv)|(\.xvid)", str(movie))
-        print(movie, re_ext)
+media_index_test = csv.reader(open(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX-TEST.csv'))
+media_index_test_list = list(csv.reader(open(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX-TEST.csv')))
+
+movie_title = media_index_test_list[0]
+movie_year = media_index_test_list[1]
+movie_resolution = media_index_test_list[2]
+movie_resolution_alt_1 = media_index_test_list[3]
+movie_resolution_alt_2 = media_index_test_list[4]
+movie_parts = media_index_test_list[5]
+movie_file_type = media_index_test_list[6]
 
 
-search_movie_folders_items()
+def movie_file_query_and_sort():
+    for movie_file in media_index_test_list:
+        print(movie_file)
+
+
+movie_file_query_and_sort()
+
 
