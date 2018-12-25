b033389210ce0535180af72c52f595c88b0916fa
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-24 20:38:44 -0500

    Add files via upload

--- CODE/RE-DIR-SEARCH-CSV.py
+++ CODE/RE-DIR-SEARCH-CSV.py
@@ -3,12 +3,9 @@ import os
 import re
 
 movie_data = os.walk(r"/run/user/1000/gvfs/smb-share:server=10.0.0.3,share=bx-movies/MOVIES/")
-test_data = os.walk(r"/home/bx/Videos/CHASE/TEST/")
-
 
 movie_walk = []
 movie_results = []
-#movie_results_sorted = sorted(movie_results)
 
 
 def search_movie_folders_items():
