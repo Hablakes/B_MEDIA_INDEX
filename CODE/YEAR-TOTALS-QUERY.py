b6b1492afba9163a8e5fd110bd71441886eb686b
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-26 11:47:07 -0500

    Add files via upload

--- CODE/YEAR-TOTALS-QUERY.py
+++ CODE/YEAR-TOTALS-QUERY.py
@@ -10,12 +10,6 @@ tv_dir = os.listdir(r"/home/bx/Videos/CHASE/TV/")
 movie_years_range = range(1900, 2100, 1)
 tv_years_range = range(1900, 2100, 1)
 
-
-tv_years_amount_dict = {}
-
-movie_amounts_list = []
-tv_amounts_list = []
-
 movie_string = str("MOVIE")
 tv_string = str("TV")
 
@@ -47,7 +41,7 @@ def get_movie_years_for_dict():
     movie_totals_query_action = input("ENTER #")
     movie_totals_query_action = int(movie_totals_query_action)
     for media_movie in media_index:
-#        media_movie_year = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(media_movie), flags=0)
+        #        media_movie_year = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(media_movie), flags=0)
         media_movie_year = int(media_movie[2])
         if movie_string in media_movie:
             if media_movie_year in movie_years_range:
@@ -57,7 +51,6 @@ def get_movie_years_for_dict():
     media_movie_year_totals = {}
     for movie_year_values, value in sorted(movie_years_amount_dict.items()):
         media_movie_year_totals[movie_year_values] = len(value)
-    #   print(media_movie_year_totals)
     movie_data = sorted(media_movie_year_totals.items())
     for movie_year_query in movie_data:
         if movie_totals_query_action in movie_year_query:
@@ -65,6 +58,7 @@ def get_movie_years_for_dict():
 
 
 def get_movie_titles_amount():
+    movie_amounts_list = []
     for counted_movie_title in media_index:
         if movie_string in counted_movie_title:
             movie_amounts_list.append(counted_movie_title)
@@ -72,10 +66,11 @@ def get_movie_titles_amount():
 
 
 def get_tv_years_for_dict():
+    tv_years_amount_dict = {}
     tv_totals_query_action = input("ENTER #")
     tv_totals_query_action = int(tv_totals_query_action)
     for media_tv in media_index:
-#        media_tv_year = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(media_tv), flags=0)
+        #        media_tv_year = re.split("(.+) \((\d{4})\) \((.+)x(.+)\)\.(.+)", str(media_tv), flags=0)
         media_tv_year = int(media_tv[2])
         if tv_string in media_tv:
             if media_tv_year in tv_years_range:
@@ -85,7 +80,6 @@ def get_tv_years_for_dict():
     media_tv_year_totals = {}
     for tv_year_values, value in sorted(tv_years_amount_dict.items()):
         media_tv_year_totals[tv_year_values] = len(value)
-    #   print(media_tv_year_totals)
     tv_data = sorted(media_tv_year_totals.items())
     for tv_year_query in tv_data:
         if tv_totals_query_action in tv_year_query:
@@ -93,10 +87,12 @@ def get_tv_years_for_dict():
 
 
 def get_tv_titles_amount():
+    tv_amounts_list = []
     for counted_tv_title in media_index:
         if tv_string in counted_tv_title:
             tv_amounts_list.append(counted_tv_title)
     print(len(tv_amounts_list))
 
 
-totals_query()
+while True:
+    totals_query()
