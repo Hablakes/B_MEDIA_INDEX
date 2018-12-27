6bea61e34717dc1a75d3f992909b2dd29295eb35
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-27 16:52:04 -0500

    Delete TESTING-1.py

--- NOTES/TESTING-1.py
+++
@@ -1,5 +0,0 @@
-extensions = (".3gp", ".asf", ".asx", ".avc", ".avi", ".bdmv", ".bin", ".bivx", ".dat", ".disc", ".divx", ".dv",
-              ".dvr-ms", ".evo", ".fli", ".flv", ".h264", ".img", ".iso", ".m2ts", ".m2v", ".m4v", ".mkv", ".mov",
-              ".mp4", ".mpeg", ".mpg", ".mt2s", ".mts", ".nrg", ".nsv", ".nuv", ".ogm", ".pva", ".qt", ".rm", ".rmvb",
-              ".srt", ".strm", ".svq3", ".ts", ".ty", ".viv", ".vob", ".vp3", ".wmv", ".xvid", ".webm")
-
