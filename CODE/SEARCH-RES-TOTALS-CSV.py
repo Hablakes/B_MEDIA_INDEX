493ea3de73eb7a8b289d29347d1c01c3ce218bcd
Blair OT <45849760+Hablakes@users.noreply.github.com>
2018-12-27 01:00:03 -0500

    Add files via upload

--- CODE/SEARCH-RES-TOTALS-CSV.py
+++ CODE/SEARCH-RES-TOTALS-CSV.py
@@ -8,6 +8,8 @@ def search_resolution_totals_movies():
     ten_eighty_found_list = []
     seven_twenty_found_list = []
     standard_def_found_list = []
+    empty_response_list = []
+    movies_total_list = []
 
     for res in media_index_test:
 
@@ -17,6 +19,10 @@ def search_resolution_totals_movies():
             seven_twenty_found_list.append(res)
         if re.findall("'\d{3}x", res[2]):
             standard_def_found_list.append(res)
+        if str("x") not in res[2]:
+            empty_response_list.append(+1)
+        if str() in res[2]:
+            movies_total_list.append(+1)
 
     print()
     print("-------------------------------------------------------------------------------------------------------")
@@ -39,6 +45,18 @@ def search_resolution_totals_movies():
     print()
     print("-------------------------------------------------------------------------------------------------------")
     print()
+    print("NUMBER:# OF MOVIES WITHOUT A MARKED RESOLUTION:")
+    print()
+    print(len(empty_response_list))
+    print()
+    print("-------------------------------------------------------------------------------------------------------")
+    print("-------------------------------------------------------------------------------------------------------")
+    print()
+    print("NUMBER:# OF MOVIES TOTAL:")
+    print()
+    print(len(movies_total_list))
+    print()
+    print("-------------------------------------------------------------------------------------------------------")
 
 
 search_resolution_totals_movies()
