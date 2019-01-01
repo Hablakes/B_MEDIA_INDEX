abe5ce6e4b17e151a14e7ca0c26a2dcab1321b37
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-01 16:07:10 -0500

    Add files via upload

--- NOTES/TESTING-1.py
+++ NOTES/TESTING-1.py
@@ -1,25 +1,23 @@
+import csv
+
 import pymediainfo
 
+tv_index = csv.reader(list(open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/TV-FILES-INDEX-TEST.csv")))
 test = pymediainfo.MediaInfo.parse(
-    "/home/bx/Videos/TEST/Sciencephile The Ai (2016)/Sciencephile The AI - Time Traveler's Handbook.mp4")
+    r"/home/bx/Videos/TEST/Sciencephile The Ai (2016)/Sciencephile The AI - Time Traveler's Handbook.mp4")
 
 
 def scrape_file_info_from_list():
-    file_titles = []
-    file_codecs = []
-    file_resolutions = []
-    file_frame_rates = []
+    tv_file_results = []
 
     for track in test.tracks:
-        file_titles.append(track.other_file_name)
-        file_codecs.append(track.codec_id_info)
-        file_resolutions.append([track.sampled_width, track.sampled_height])
-        file_frame_rates.append(track.frame_rate)
+        tv_file_results.append(
+            [track.other_file_name, track.sampled_width, track.sampled_height, track.file_extention])
 
-    print(file_titles)
-    print(file_codecs)
-    print(file_resolutions)
-    print(file_frame_rates)
+    with open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/TV-FILES-RESULTS-TEST.csv", "w", newline="") as f:
+        csv_writer = csv.writer(f)
+        for tv_row in tv_file_results:
+            csv_writer.writerow(tv_row)
 
 
 scrape_file_info_from_list()
