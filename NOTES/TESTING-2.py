fb62b04448c865459325ca96b74b6e5201ff3b07
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-03 14:00:38 -0500

    Add files via upload

--- NOTES/TESTING-2.py
+++ NOTES/TESTING-2.py
@@ -19,8 +19,8 @@ def scrape_file_info_from_list():
         for track in test.tracks:
             if track.track_type == 'Video':
                 tv_file_results.append(
-                    [title['title'], title['season'], str(track.width) + 'x' + str(track.height), title['year'],
-                     title['container']])
+                    [title['title'], title['episode_title'], title['season'], title['episode'],
+                     str(track.width) + 'x' + str(track.height), title['year'], title['container']])
 
     with open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/TV-FILES-RESULTS-TEST.csv", "w", newline="") as f:
         csv_writer = csv.writer(f)
