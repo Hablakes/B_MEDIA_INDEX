4ff4434c8d96fa33b55c84f0e1c9aba7b9188852
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-03 08:31:01 -0500

    Add files via upload

--- NOTES/TESTING-1.py
+++ NOTES/TESTING-1.py
@@ -2,17 +2,18 @@ import csv
 
 import pymediainfo
 
+import guessit
+
+print(guessit.guessit("Treme.1x03.Right.Place,.Wrong.Time.HDTV.XviD-NoTV.avi"))
+
 tv_index = csv.reader(open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/INDEX-TEST.csv"))
 
 
 def scrape_file_info_from_list():
-    tv_file_results = []
-
     for tv_file in tv_index:
-
         test = pymediainfo.MediaInfo.parse(tv_file[0])
-
         for track in test.tracks:
             print(track.to_data())
+
 
 scrape_file_info_from_list()
