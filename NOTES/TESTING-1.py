ebbf31cbe27711d0795ca49ce2086a2a6f3a2071
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-03 01:08:16 -0500

    Add files via upload

--- NOTES/TESTING-1.py
+++ NOTES/TESTING-1.py
@@ -2,9 +2,7 @@ import csv
 
 import pymediainfo
 
-tv_index = csv.reader(list(open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/TV-FILES-INDEX-TEST.csv")))
-test = pymediainfo.MediaInfo.parse(
-    r"/home/bx/Videos/TEST/Honest Trailers (2012)/Honest Trailers - 801 - Venom.mp4")
+tv_index = csv.reader(open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/INDEX-TEST.csv"))
 
 
 def scrape_file_info_from_list():
@@ -14,15 +12,18 @@ def scrape_file_info_from_list():
     tv_file_height = []
     tv_file_file_extension = []
 
-    for track in test.tracks:
-        tv_file_other_file_name.append(track.other_file_name)
-        tv_file_width.append(track.width)
-        tv_file_height.append(track.height)
-        tv_file_file_extension.append(track.file_extension)
+    for tv_file in tv_index:
+
+        test = pymediainfo.MediaInfo.parse(tv_file[0])
+
+        for track in test.tracks:
+            tv_file_other_file_name.append(track.other_file_name)
+            tv_file_width.append(track.width)
+            tv_file_height.append(track.height)
+            tv_file_file_extension.append(track.file_extension)
 
     tv_file_results.append(
         [tv_file_other_file_name[0], [tv_file_width[1], tv_file_height[1]], [tv_file_file_extension[0]]])
-    print(tv_file_results[0])
 
     with open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/TV-FILES-RESULTS-TEST.csv", "w", newline="") as f:
         csv_writer = csv.writer(f)
