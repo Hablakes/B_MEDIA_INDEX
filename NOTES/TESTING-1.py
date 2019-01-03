f258564e7e357afbf96dd111a13d1cdd4c6c8e5f
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-03 01:51:59 -0500

    Add files via upload

--- NOTES/TESTING-1.py
+++ NOTES/TESTING-1.py
@@ -7,28 +7,12 @@ tv_index = csv.reader(open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/INDEX-
 
 def scrape_file_info_from_list():
     tv_file_results = []
-    tv_file_other_file_name = []
-    tv_file_width = []
-    tv_file_height = []
-    tv_file_file_extension = []
 
     for tv_file in tv_index:
 
         test = pymediainfo.MediaInfo.parse(tv_file[0])
 
         for track in test.tracks:
-            tv_file_other_file_name.append(track.other_file_name)
-            tv_file_width.append(track.width)
-            tv_file_height.append(track.height)
-            tv_file_file_extension.append(track.file_extension)
-
-    tv_file_results.append(
-        [tv_file_other_file_name[0], [tv_file_width[1], tv_file_height[1]], [tv_file_file_extension[0]]])
-
-    with open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/TV-FILES-RESULTS-TEST.csv", "w", newline="") as f:
-        csv_writer = csv.writer(f)
-        for tv_row in tv_file_results:
-            csv_writer.writerow(tv_row)
-
+            print(track.to_data())
 
 scrape_file_info_from_list()
