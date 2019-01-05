cfd1f0090ed5f7d242069387a6c677ec87f23784
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-05 15:03:18 -0500

    Add files via upload

--- RUN/LAUNCH-MEDIA-INDEX-NIX.py
+++ RUN/LAUNCH-MEDIA-INDEX-NIX.py
@@ -3,6 +3,7 @@ import os
 import re
 
 import matplotlib.pylab as plt
+import numpy as np
 
 media_index = csv.reader(open(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv'))
 media_index_list = list(csv.reader(open(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MEDIA-INDEX.csv')))
@@ -216,6 +217,98 @@ def get_tv_years_decades_totals_graphs():
     plt.show()
 
 
+def search_resolution_totals_movies():
+    fig, ax = plt.subplots(figsize=(20, 10), subplot_kw=dict(aspect="equal"))
+
+    ten_eighty_found_list = []
+    seven_twenty_found_list = []
+    standard_def_found_list = []
+    empty_response_list = []
+    movies_total_list = []
+
+    for res in movie_files_results:
+
+        if re.findall("19\d{2}x", res[2]):
+            ten_eighty_found_list.append(res)
+        elif re.findall("1[0-8]\d{2}x", res[2]):
+            seven_twenty_found_list.append(res)
+        elif re.findall("\d{3}x", res[2]):
+            standard_def_found_list.append(res)
+        else:
+            empty_response_list.append(+1)
+        movies_total_list.append(+1)
+
+    labels = ['1080p', '720p', 'SD (Below 720p)']
+
+    data = [float(len(ten_eighty_found_list)), float(len(seven_twenty_found_list)),
+            float(len(standard_def_found_list))]
+
+    def format_movie_data(pct, allvals):
+        absolute = int(pct / 100. * np.sum(allvals))
+        return "{:.1f}%\n({:d})".format(pct, absolute)
+
+    colors = ['skyblue', 'cyan', 'magenta']
+
+    wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: format_movie_data(pct, data),
+                                      shadow=True, colors=colors, textprops=dict(color="black"))
+
+    ax.legend(wedges, labels,
+              title="RESOLUTIONS",
+              loc="center left",
+              bbox_to_anchor=(1, 0, 0.5, 1))
+
+    plt.setp(autotexts, size=9, weight='bold')
+    ax.set_title("MOVIE-RESOLUTION-RESULTS")
+    plt.savefig(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/MOVIE-RESOLUTION-RESULTS.png')
+    plt.show()
+
+
+def search_resolution_totals_tv_shows():
+    fig, ax = plt.subplots(figsize=(20, 10), subplot_kw=dict(aspect="equal"))
+
+    ten_eighty_found_list = []
+    seven_twenty_found_list = []
+    standard_def_found_list = []
+    empty_response_list = []
+    movies_total_list = []
+
+    for res in tv_files_results:
+
+        if re.findall("19\d{2}x", res[5]):
+            ten_eighty_found_list.append(res)
+        elif re.findall("1[0-8]\d{2}x", res[5]):
+            seven_twenty_found_list.append(res)
+        elif re.findall("\d{3}x", res[5]):
+            standard_def_found_list.append(res)
+        else:
+            empty_response_list.append(+1)
+        movies_total_list.append(+1)
+
+    labels = ['1080p', '720p', 'SD (Below 720p)']
+
+    data = [float(len(ten_eighty_found_list)), float(len(seven_twenty_found_list)),
+            float(len(standard_def_found_list))]
+
+    def format_movie_data(pct, allvals):
+        absolute = int(pct / 100. * np.sum(allvals))
+        return "{:.1f}%\n({:d})".format(pct, absolute)
+
+    colors = ['skyblue', 'cyan', 'magenta']
+
+    wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: format_movie_data(pct, data),
+                                      shadow=True, colors=colors, textprops=dict(color="black"))
+
+    ax.legend(wedges, labels,
+              title="RESOLUTIONS",
+              loc="center left",
+              bbox_to_anchor=(1, 0, 0.5, 1))
+
+    plt.setp(autotexts, size=9, weight='bold')
+    ax.set_title("TV-SHOW-RESOLUTION-RESULTS")
+    plt.savefig(r'/home/bx/PycharmProjects/B-MEDIA-INDEX/FILES/TV-SHOW-RESOLUTION-RESULTS.png')
+    plt.show()
+
+
 def scrape_movie_info_for_csv():
     for movie_found in movie_dir_list:
         movie_scrape_info = re.search("(.+) \((\d{4})\)", str(movie_found), flags=0)
@@ -416,9 +509,11 @@ def run_graphs():
     print()
     print("--------------------------------------------------------------------------------------------------")
     print()
-    print("1) MOVIES (TITLES PER YEAR)   - 2) TV SHOWS (TITLES PER YEAR)")
+    print("1) MOVIES (TITLES PER YEAR)         - 2) TV SHOWS (TITLES PER YEAR)")
     print()
-    print("3) MOVIES (TITLES PER DECADE) - 4) TV SHOWS (TITLES PER DECADE)                      - 5) EXIT")
+    print("3) MOVIES (TITLES PER DECADE)       - 4) TV SHOWS (TITLES PER DECADE)")
+    print()
+    print("5) MOVIES (RESOLUTIONS PERCENTAGES) - 6) TV SHOWS (RESOLUTIONS PERCENTAGES)          - 7) EXIT")
     print()
     print("--------------------------------------------------------------------------------------------------")
     print()
@@ -436,6 +531,10 @@ def run_graphs():
     elif graph_options == 4:
         get_tv_years_decades_totals_graphs()
     elif graph_options == 5:
+        search_resolution_totals_movies()
+    elif graph_options == 6:
+        search_resolution_totals_tv_shows()
+    elif graph_options == 7:
         launch_media_index()
 
 
