0e4ee3a7986b35df26afcba2589db9f789b3bad7
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-05 19:48:33 -0500

    Add files via upload

---
+++ NOTES/TESTING-1.py
@@ -0,0 +1,32 @@
+import csv
+
+import guessit
+import pymediainfo
+
+movie_index = csv.reader(open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/NOTES/INDEX-TEST.csv"))
+tv_index = csv.reader(open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/NOTES/INDEX-TEST.csv"))
+
+
+def get_tv_show_index_results():
+    tv_index_file_results = []
+
+    for tv_file in tv_index:
+
+        title = guessit.guessit(tv_file[0], options={'type': 'episode', 'episode-prefer-number': True})
+
+        test = pymediainfo.MediaInfo.parse(tv_file[0])
+
+        for track in test.tracks:
+
+            if track.track_type == 'Video':
+                tv_index_file_results.append(
+                    [title.get('title'), title.get('episode_title'), title.get('season'), title.get('episode'),
+                     title.get('year'), str(track.width) + 'x' + str(track.height), title.get('container')])
+
+    with open(r"/home/bx/PycharmProjects/B-MEDIA-INDEX/NOTES/FILES-RESULTS-TEST.csv", "w", newline="") as f:
+        csv_writer = csv.writer(f)
+        for tv_row in sorted(tv_index_file_results):
+            csv_writer.writerow(tv_row)
+
+
+get_tv_show_index_results()
