98663309e6c838aedfe30a031b79fadf0ef2ec16
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-18 08:53:00 -0500

    Add files via upload

--- CODE/RESULTS_DIFFERENCES.py
+++ CODE/RESULTS_DIFFERENCES.py
@@ -2,7 +2,7 @@
 
 def compare_results_files_and_create_differences_files(username_input, username_input_alt):
 
-    username_input_alt = input("ENTER USERNAME FOR RESULTS LISTS TO COMPARE")
+    username_input_alt = input("ENTER USERNAME FOR RESULTS LISTS TO COMPARE:")
 
     with open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/MOVIE-FILES-RESULTS.csv',
               'r') as mr_0, open(
