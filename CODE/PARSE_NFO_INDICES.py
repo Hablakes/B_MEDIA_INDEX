22d8100570e033a9e50381814cacd996adcc5a8c
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-19 13:13:12 -0500

    Add files via upload

--- CODE/PARSE_NFO_INDICES.py
+++ CODE/PARSE_NFO_INDICES.py
@@ -7,21 +7,40 @@ def movie_index_results(username_input):
     movie_nfo_file_results = []
 
     nfo_file = input("SEARCH:")
+    print()
+    print("--------------------------------------------------------------------------------------------------")
+    print()
 
     for nfo_files_index in movie_nfos:
         for nfo_files in nfo_files_index:
-            if nfo_file in nfo_files:
+            if nfo_file.lower() in nfo_files.lower():
                 movie_nfo_file_results.append(nfo_files)
 
-            if int(len(movie_nfo_file_results)) > 1:
-                print("REFINE SEARCH WITH ABOVE RESULTS: SINGLE MATCH NEEDED TO PROCEED")
-                movie_index_results(username_input)
+    if int(len(movie_nfo_file_results)) == 0:
+        print("NO MATCHES")
+        print()
+        print("--------------------------------------------------------------------------------------------------")
+        print()
+        exit()
 
-            else:
-                with open(nfo_files) as f:
-                    for line in f:
-                        print(line)
+    if int(len(movie_nfo_file_results)) > 1:
+        for line in movie_nfo_file_results:
+            print(line)
+            print()
+            print("--------------------------------------------------------------------------------------------------")
+            print()
+        print("REFINE SEARCH WITH ABOVE RESULTS: SINGLE MATCH NEEDED TO PROCEED")
+        print()
+        print("--------------------------------------------------------------------------------------------------")
+        print()
+        movie_index_results(username_input)
+    else:
+        with open(movie_nfo_file_results[0]) as f:
+            for line in f.readlines():
+                print(line)
+        print()
+        print("--------------------------------------------------------------------------------------------------")
+        print()
 
 
 movie_index_results(username_input='bx')
-
