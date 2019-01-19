cb7fa00f01624f214d7b300f697d70dafa380139
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-19 18:58:05 -0500

    Add files via upload

--- CODE/TEST_FOLDERS.py
+++ CODE/TEST_FOLDERS.py
@@ -3,20 +3,16 @@ import os
 
 
 def create_movie_folders(username_input):
-    media_index = csv.reader(
-        open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/FILES/MEDIA-INDEX.csv'))
     media_index_list = list(
-        csv.reader(open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/FILES/MEDIA-INDEX.csv')))
+        csv.reader(open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/MEDIA-INDEX.csv')))
     for m_title in media_index_list:
         if str("MOVIE") in m_title:
             os.makedirs('/home/bx/Videos/CHASE/MOVIES/' + m_title[1] + " (" + m_title[2] + ")", exist_ok=True)
 
 
 def create_tv_folders(username_input):
-    media_index = csv.reader(
-        open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/FILES/MEDIA-INDEX.csv'))
     media_index_list = list(
-        csv.reader(open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/FILES/MEDIA-INDEX.csv')))
+        csv.reader(open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/MEDIA-INDEX.csv')))
     for tv_title in media_index_list:
         if str("TV") in tv_title:
             os.makedirs('/home/bx/Videos/CHASE/TV/' + tv_title[1] + " (" + tv_title[2] + ")", exist_ok=True)
