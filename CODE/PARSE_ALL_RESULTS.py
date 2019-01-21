9cf965034be0a9d5bcf335bc98f342c50a9549e9
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-01-21 17:48:56 -0500

    Add files via upload

--- CODE/PARSE_ALL_RESULTS.py
+++ CODE/PARSE_ALL_RESULTS.py
@@ -16,11 +16,11 @@ def movie_index_all_results(username_input):
     movie_index = csv.reader(
         open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/MOVIE-INDEX.csv'))
 
-    movie_index_file_results = []
-    movie_nfo_results = []
-    movie_srt_results = []
+    movie_index_file_results = {}
 
     for movie_file in sorted(movie_index):
+
+        title_key = movie_file[0].rsplit('/')[-2]
 
         if movie_file[0].endswith(extensions):
 
@@ -31,7 +31,10 @@ def movie_index_all_results(username_input):
             for track in test.tracks:
 
                 if track.track_type == 'Video':
-                    movie_index_file_results.append(
+
+                    if title_key not in movie_index_file_results:
+                        movie_index_file_results[title_key] = []
+                    movie_index_file_results[title_key].append(
                         [title.get('title'), title.get('year'), str(track.width) + 'x' + str(track.height),
                          title.get('container')])
 
@@ -39,20 +42,15 @@ def movie_index_all_results(username_input):
             with open(movie_file[0]) as f:
                 for line in f.readlines():
                     if '<plot>' in line:
-                        movie_nfo_results.append([movie_file[0].rsplit('/')[-2], '', '', '', '', '', '', '', '', '',
-                                                  line.split('.')])
+                        movie_index_file_results[title_key].append([line.split('.')])
 
         elif movie_file[0].endswith(srt_extensions):
-            movie_srt_results.append([movie_file[0].rsplit('/')[-2], '', '', '', '', '', '', '', '', "SRT AVAILABLE"])
+            movie_index_file_results[title_key].append(["SRT AVAILABLE"])
 
     with open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/MOVIE-RESULTS.csv', "w",
               newline="") as f:
         csv_writer = csv.writer(f)
-        for movie_row in movie_index_file_results:
-            csv_writer.writerow(movie_row)
-        for movie_row in movie_nfo_results:
-            csv_writer.writerow(movie_row)
-        for movie_row in movie_srt_results:
+        for movie_row in movie_index_file_results.items():
             csv_writer.writerow(movie_row)
 
 
@@ -60,11 +58,11 @@ def tv_index_all_results(username_input):
     tv_index = csv.reader(
         open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/TV-INDEX.csv'))
 
-    tv_index_file_results = []
-    tv_nfo_results = []
-    tv_srt_results = []
+    tv_index_file_results = {}
 
     for tv_file in sorted(tv_index):
+
+        title_key = tv_file[0].rsplit('/')[-2]
 
         if tv_file[0].endswith(extensions):
 
@@ -77,31 +75,29 @@ def tv_index_all_results(username_input):
             for track in test.tracks:
 
                 if track.track_type == 'Video':
-                    tv_index_file_results.append(
+
+                    if title_key not in tv_index_file_results:
+                        tv_index_file_results[title_key] = []
+                        tv_index_file_results[title_key].append(
                         [title_and_year, title.get('title'), title.get('episode_title'), title.get('season'),
                          title.get('episode'), title.get('year'), str(track.width) + 'x' + str(track.height),
                          title.get('container')])
+
+        elif tv_file[0].endswith(srt_extensions):
+            tv_index_file_results[title_key].append(["SRT AVAILABLE"])
 
         elif tv_file[0].endswith(nfo_extensions):
             with open(tv_file[0]) as f:
                 for line in f.readlines():
                     if '<plot>' in line:
-                        tv_nfo_results.append([tv_file[0].rsplit('/')[-2], '', '', '', '', '', '', '', '', '',
-                                                  line.split('.')])
-
-        elif tv_file[0].endswith(srt_extensions):
-            tv_srt_results.append([tv_file[0].rsplit('/')[-2], '', '', '', '', '', '', '', '', "SRT AVAILABLE"])
+                        tv_index_file_results[title_key].append([line.split('.')])
 
     with open(r'/home/' + username_input + '/' + username_input + '-MEDIA-INDEX/TV-RESULTS.csv', "w",
               newline="") as f:
         csv_writer = csv.writer(f)
-        for tv_row in sorted(tv_index_file_results):
-            csv_writer.writerow(tv_row)
-        for tv_row in sorted(tv_nfo_results):
-            csv_writer.writerow(tv_row)
-        for tv_row in sorted(tv_srt_results):
+        for tv_row in tv_index_file_results.items():
             csv_writer.writerow(tv_row)
 
 
-movie_index_all_results(username_input='bx')
-#tv_index_all_results(username_input='bx')
+#movie_index_all_results(username_input='bx')
+tv_index_all_results(username_input='bx')
