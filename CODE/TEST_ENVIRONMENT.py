6a612003c07f2617fadada470d2326042cb3ad77
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-07-27 16:07:30 -0400

    Add files via upload

--- CODE/TEST_ENVIRONMENT.py
+++ CODE/TEST_ENVIRONMENT.py
@@ -1,44 +1,48 @@
 import os
 
+import pyfiglet
+from tkinter import filedialog, Tk
 
-from tkinter import filedialog
-from tkinter import *
 
-directory_selected_in_function = []
+def main():
+    create_test_folders()
 
 
 def get_directory_to_scan():
-    print()
-    print("-" * 100)
-    print("___  _  _    ___ ____ ____ ___    ____ ____ _    ___  ____ ____ ____")
-    print("|__]  \/  __  |  |___ [__   |  __ |___ |  | |    |  \ |___ |__/ [__ ")
-    print("|__] _/\_     |  |___ ___]  |     |    |__| |___ |__/ |___ |  \ ___]")
-    print()
-    print("-" * 100)
-    print()
+    separator_2()
+    print(pyfiglet.figlet_format('TEST', font='cybermedium'))
+    print(pyfiglet.figlet_format('ENVIRONMENT', font='cybermedium'))
+    separator_3()
     root = Tk()
     root.withdraw()
     root.update()
     selected_directory = filedialog.askdirectory()
-    directory_selected_in_function.append(selected_directory)
     root.destroy()
-    print("-" * 100)
-    print("-" * 100)
-    print()
-    print()
     print("DIRECTORY INPUT: ", selected_directory)
-    print()
-    print()
-    print("-" * 100)
-    print("-" * 100)
-    print()
+    separator_3()
+    return selected_directory
 
 
 def create_test_folders():
-    get_directory_to_scan()
+    directory_selected_in_function = [get_directory_to_scan()]
 
     for items in os.listdir(directory_selected_in_function[0]):
         os.makedirs('/home/bx/Videos/CHASE/TEMP/' + items, exist_ok=True)
 
 
-create_test_folders()
+def separator_1():
+    print('-' * 100)
+
+
+def separator_2():
+    for items in '\n', '-' * 100:
+        print(items)
+
+
+def separator_3():
+    for items in '\n', '-' * 100, '\n':
+        print(items)
+
+
+if __name__ == '__main__':
+    main()
