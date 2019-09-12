aa715ae7f89b125e9eeabe0351a16c6cc4d30ebf
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-09-11 22:32:39 -0400

    Add files via upload

--- RUN/B_M_I_V4.py
+++ RUN/B_M_I_V4.py
@@ -1241,7 +1241,7 @@ def scrape_media_folders_for_csv():
 
     try:
 
-        if movie_dir_input is not str(''):
+        if movie_dir_input != '':
             movie_dir_list = os.listdir(movie_dir_input)
             for movie_found in sorted(movie_dir_list):
                 movie_scrape_info = guessit.guessit(movie_found)
@@ -1252,18 +1252,22 @@ def scrape_media_folders_for_csv():
                     title_item_check.remove(title_item_check[2])
                 movie_title_items.append(title_item_check)
 
-        elif movie_alt_dir_input is not str(''):
-            movie_alt_dir_list = os.listdir(movie_alt_dir_input)
-            for movie_found in sorted(movie_alt_dir_list):
-                movie_scrape_info = guessit.guessit(movie_found)
-                title_item_check = ['MOVIE', movie_scrape_info.get('title'), str(movie_scrape_info.get('year'))]
+        if movie_alt_dir_input != '':
+            found_alt_movie_directories_list = []
+            for alternate_movie_directories in movie_alt_dir_input:
+                movie_alt_dir_list = os.listdir(alternate_movie_directories)
+                for found_alt_movie_directories in movie_alt_dir_list:
+                    found_alt_movie_directories_list.append(found_alt_movie_directories)
+                for movie_found in sorted(found_alt_movie_directories_list):
+                    movie_scrape_info = guessit.guessit(movie_found)
+                    title_item_check = ['MOVIE', movie_scrape_info.get('title'), str(movie_scrape_info.get('year'))]
 
-                if "," in title_item_check[2]:
-                    title_item_check.append(title_item_check[2][-5:-1])
-                    title_item_check.remove(title_item_check[2])
-                movie_title_items.append(title_item_check)
+                    if "," in title_item_check[2]:
+                        title_item_check.append(title_item_check[2][-5:-1])
+                        title_item_check.remove(title_item_check[2])
+                    movie_title_items.append(title_item_check)
 
-        elif tv_dir_input is not str(''):
+        if tv_dir_input != '':
             tv_dir_list = os.listdir(tv_dir_input)
             for tv_found in sorted(tv_dir_list):
                 tv_scrape_info = guessit.guessit(tv_found)
@@ -1274,16 +1278,20 @@ def scrape_media_folders_for_csv():
                     title_item_check.remove(title_item_check[2])
                 tv_title_items.append(title_item_check)
 
-        elif tv_alt_dir_input is not str(''):
-            tv_alt_dir_list = os.listdir(tv_alt_dir_input)
-            for tv_found in sorted(tv_alt_dir_list):
-                tv_scrape_info = guessit.guessit(tv_found)
-                title_item_check = ['TV', tv_scrape_info.get('title'), str(tv_scrape_info.get('year'))]
+        if tv_alt_dir_input != '':
+            found_alt_tv_directories_list = []
+            for alternate_tv_directories in tv_alt_dir_input:
+                tv_alt_dir_list = os.listdir(alternate_tv_directories)
+                for found_alt_tv_directories in tv_alt_dir_list:
+                    found_alt_tv_directories_list.append(found_alt_tv_directories)
+                for tv_found in sorted(found_alt_tv_directories_list):
+                    tv_scrape_info = guessit.guessit(tv_found)
+                    title_item_check = ['TV', tv_scrape_info.get('title'), str(tv_scrape_info.get('year'))]
 
-                if "," in title_item_check[2]:
-                    title_item_check.append(title_item_check[2][-5:-1])
-                    title_item_check.remove(title_item_check[2])
-                tv_title_items.append(title_item_check)
+                    if "," in title_item_check[2]:
+                        title_item_check.append(title_item_check[2][-5:-1])
+                        title_item_check.remove(title_item_check[2])
+                    tv_title_items.append(title_item_check)
 
         with open(os.path.expanduser((index_folder + '/MEDIA_TITLE_INDEX.csv').format(username)), 'w',
                   encoding='UTF-8', newline='') as f:
