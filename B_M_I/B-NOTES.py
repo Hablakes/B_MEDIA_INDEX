715dd893557c44e631a11fa16368b87a6d53e24b
Blair OT <45849760+Hablakes@users.noreply.github.com>
2020-01-13 16:55:21 -0500

    Add files via upload

---
+++ B_M_I/B-NOTES.py
@@ -0,0 +1,66 @@
+"""
+----------------------------------------------------------------------------------
+PIP INSTALLS -
+----------------------------------------------------------------------------------
+
+UPGRADE **
+
+ascii-graph
+guessit
+matplotlib
+pyfiglet
+pymediainfo
+
+----------------------------------------------------------------------------------
+
+aplotter
+asciichart
+asciiplotlib
+babblefish
+bashplotlib
+click
+enzyme
+get-media-files
+gnuplot
+guess-language
+rebulk
+termgraph
+
+----------------------------------------------------------------------------------
+OS INSTALLS -
+----------------------------------------------------------------------------------
+
+Use Terminal: sudo pacman -S * (on Arch based systems)
+
+* = NIX
+
+bashplotlib
+cool-retro-term
+cmatrix
+gnuplot, gnuplot-git
+libmediainfo
+lolcat
+mediainfo, mediainfo-gui
+neofetch
+screenfetch
+tk
+tmux
+
+----------------------------------------------------------------------------------
+
+* = WIN
+
+MediaInfo (https://mediaarea.net/en/MediaInfo)
+
+----------------------------------------------------------------------------------
+MISC -
+----------------------------------------------------------------------------------
+
+LAUNCH FROM CMD -
+
+C:\Users\botoole\Downloads\B\BPT\B-MEDIA-INDEX\venv\Scripts\python.exe
+ C:\Users\botoole\Downloads\B\BPT\B-MEDIA-INDEX\RUN\LAUNCH-MEDIA-INDEX-WIN.py
+
+MAKE SURE ABOVE LINE IS ALL IN ONE LINE FOR CMD*
+----------------------------------------------------------------------------------
+"""
